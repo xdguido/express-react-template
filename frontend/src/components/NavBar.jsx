14fac165347b5f30052adcbc5af38322c7201095
xdguido <guidogennari95@gmail.com>
2022-10-24 00:49:30 -0300

    Add email to profile dropdown

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -118,7 +118,10 @@ function NavBar() {
                                             leaveFrom="transform opacity-100 scale-100"
                                             leaveTo="transform opacity-0 scale-95"
                                         >
-                                            <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                            <Menu.Items className="absolute right-0 z-10 mt-2 w-min-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                                <span className="block px-4 py-2 text-sm text-gray-400">
+                                                    {user.email}
+                                                </span>
                                                 {profileNav.map((item) => (
                                                     <Menu.Item key={item.name}>
                                                         {({ active }) => (
