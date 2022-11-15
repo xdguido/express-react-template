46974dfd0358f99fb70b883039da35819f9b2b9d
xdguido <guidogennari95@gmail.com>
2022-11-15 12:22:42 -0300

    Base url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://oauth-template-app.herokuapp.com/';
+const BASE_URL = 'http://localhost:5000/';
 
 export default axios.create({
     baseURL: BASE_URL
