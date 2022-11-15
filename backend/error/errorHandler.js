9d35ce2a5ee666a04ded0da003c8f9b12ac77ed2
xdguido <guidogennari95@gmail.com>
2022-11-15 13:04:57 -0300

    debug

--- backend/error/errorHandler.js
+++ backend/error/errorHandler.js
@@ -11,8 +11,7 @@ const errorHandler = (err, req, res, next) => {
         console.error('Unknown Error occured:', err);
         res.status(500).send({
             name: ErrorCode.UnknownError,
-            // stack: NODE_ENV === 'production' ? null : err.stack
-            stack: err.stack
+            stack: NODE_ENV === 'production' ? null : err.stack
         });
     }
 };
